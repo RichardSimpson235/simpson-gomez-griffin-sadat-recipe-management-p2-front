57e05efb1943e70f98780673ecdd76b040d3501b
Richard Simpson <richard.simpson235@gmail.com>
2022-02-18 14:56:12 -0500

    feat: add login skeleton logic

--- toastr/src/App.js
+++ toastr/src/App.js
@@ -4,23 +4,37 @@ import './components/login.css';
 
 function App() {
 
-  const loginEndpoint = "";
+    const loginEndpoint = "";
 
-  const [user, setUser] = useState({});
+    const [user, setUser] = useState({});
 
-  const handleLogin = (username, password) => {
-    console.log("Username: " + username + ", " + "Password: " + password);
+    const handleLogin = (username, password) => {
+        let body = {
+            username: username,
+            password: password
+        };
+
+        const requestInit = {
+            method: 'GET',
+            body: body
+        };
+
+        fetch(loginEndpoint, requestInit).then(response => {
+            if(response.status == 200) {
+                setUser(response.json());
+            }
+        });
   }
   
   return (
     <div className="App container">
-      {
-        Object.keys(user).length === 0 ? (
-          <Login loginEndpoint={loginEndpoint} handler={handleLogin}/>
-        ) : (
-          <h1>Hello from React, {user.id}</h1>
-        )
-      }
+        {
+            Object.keys(user).length === 0 ? (
+                <Login loginEndpoint={loginEndpoint} handler={handleLogin}/>
+            ) : (
+                <h1>Hello from React, {user.id}</h1>
+            )
+        }
     </div>
   );
 }
