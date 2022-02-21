f2c0595295bd57d592aa27b8ffa65644e5e012ee
RichardSimpson235 <57775052+RichardSimpson235@users.noreply.github.com>
2022-02-21 15:46:37 -0500

    feat: add recipe search feature

--- toastr/src/components/Search.jsx
+++ toastr/src/components/Search.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import RecipeHolder from './RecipeHolder';
 
 function Search() {
+
+    const [recipes, setRecipes] = useState([]);
+    const searchString = useRef();
+
+    const searchEndpoint = new URL("http://localhost:8080/recipes/search");
+
+    const search = async (e) => {
+
+        if(e.code === "Enter") {
+            searchEndpoint.search = new URLSearchParams({
+                name: searchString.current.value
+            });
+
+            const response = await fetch(searchEndpoint);
+            const recipes = await response.json();
+
+            setRecipes(recipes);
+        }
+    }
+
     return (
         <div className='col'>
-            <p> Aliquam posuere augue sed dictum laoreet. Aenean eu bibendum neque, ut venenatis mauris. Praesent non vulputate ex. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris a magna molestie, posuere risus vel, vulputate ante. Pellentesque scelerisque tincidunt nibh in iaculis. Aliquam hendrerit placerat ante ac dictum. Ut a dui neque. Vivamus sit amet pharetra orci. In volutpat purus id hendrerit tincidunt. In aliquam maximus ultrices. Integer sed nulla quam.
+            <input onKeyUp={search} ref={searchString} className='form-control' type='text' placeholder="Search for a recipe..." />
 
-            Fusce metus urna, consequat ut vehicula eu, consectetur eget magna. Vestibulum pretium neque a turpis imperdiet sagittis. Phasellus nec orci vitae diam mollis fermentum. Donec tellus purus, convallis at malesuada ac, aliquet ut urna. Mauris a nulla massa. Nulla dictum id enim sit amet accumsan. Fusce non turpis vulputate, placerat est a, molestie velit. Morbi pulvinar, quam in faucibus ultricies, purus diam blandit tellus, a dictum mauris metus ut justo.
-
-            Aenean tincidunt metus sed turpis pretium vehicula. Morbi laoreet eu nisi vulputate tristique. Pellentesque venenatis eros et blandit vehicula. Cras sed orci id libero dictum laoreet. Fusce velit eros, condimentum et nisi ut, porttitor pretium turpis. Duis eu mi lectus. Nullam porttitor eu ante efficitur commodo. Phasellus consectetur laoreet ante, vel semper nisi cursus quis. Sed ullamcorper molestie neque, in efficitur felis scelerisque rhoncus. Nullam ac neque sem.
-            </p>
+            {
+                Object.keys(recipes).length === 0 ? "" :
+                <RecipeHolder recipes={recipes} isDeletable={false}/>
+            }
         </div>
     )
 }
